b81ef8309f7fc0e46df649224d697ca7ce19a6af
Oleg Sukhinin <dark-nord@bk.ru>
2020-10-24 18:23:39 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="block" :style="{'margin-left': left + 'px'}"></div>
+  </div>
+</template>
+
+<style lang="scss">
+.block {
+  width: 50px;
+  height: 50px;
+  background-color: #000;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      someVar: "",
+    };   
+  },
+  methods: {
+    handleIncrement() {
+      this.count = this.count + 1;
+    }
+  },
+};
+</script>
